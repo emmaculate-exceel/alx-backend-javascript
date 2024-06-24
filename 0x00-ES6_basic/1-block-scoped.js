10cbe3480c53b09bc98bf429edbdd8a474d4b891
emmaculate-exceel <emmaculate2exceel@live.com>
2024-06-24 16:14:20 +0100

    update

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
     var task = false;
     var task2 = true;
 
     if (trueOrFalse) {
-	let task = true;
-	let task = false;
+	const task = true;
+	const task = false;
     }
 
     return [task, task2];
