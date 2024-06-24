3ef22fb48eb54542681d0aeb86f206a2325e06a0
emmaculate-exceel <emmaculate2exceel@live.com>
2024-06-24 15:44:01 +0100

    blocked scoped

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+    let task = false;
+    let task2 = true;
+
+    if (trueOrFalse) {
+	let task = true;
+	let task = false;
+    }
+
+    return [task, task2];
+}
