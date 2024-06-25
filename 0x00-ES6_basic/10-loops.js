04d5fc277bd3abdfcd341b1a659bb1e524dbee84
emmaculate-exceel <emmaculate2exceel@live.com>
2024-06-25 05:58:17 +0100

    update

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,5 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
   for (let value of array) {
