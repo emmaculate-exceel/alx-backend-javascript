38ef5e1d6ba798eb04c5c1c8d07fa1cb32d8eb80
emmaculate-exceel <emmaculate2exceel@live.com>
2024-06-27 05:40:00 +0100

    update 100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -7,8 +7,10 @@ export default async function asyncUploadUser() {
     const photo = await uploadPhoto();
     const user = await createUser();
     res = { photo, user };
+	console.log(res);
   } catch (err) {
-    res = { photo: null, user: null };
+      res = { photo: null, user: null };
+	  console.log(res)
   }
   return res;
 }
