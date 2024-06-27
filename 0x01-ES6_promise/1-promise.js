5806bcc7118973d4da45cfeda33869e60ebf79c9
emmaculate-exceel <emmaculate2exceel@live.com>
2024-06-27 06:36:22 +0100

    1-promise 01

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,15 @@
+
+
+
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-	if (success) {
+    if (success) {
 	  resolve({ status: 200, body: 'Success' });
-	} else {
-	  reject(new Error('The fake API is not working currently'));
-	}
-	});
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+    });
 }
+
+
+
