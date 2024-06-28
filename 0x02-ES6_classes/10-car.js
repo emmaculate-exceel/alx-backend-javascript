e1e063ecb4048183ca1e3b53f3ed6cc9decf2b19
emmaculate-exceel <emmaculate2exceel@live.com>
2024-06-28 02:00:06 +0100

    10-cars1

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,11 +1,11 @@
 export default class Car{
   constructor (brand, motor, color) {
-    this._brand = brand,
-    this._motor = motor,
-    this._color = color
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
 
   cloneCar() {
-    return Object.create(this)
+    return Object.create(this);
   }
 }
