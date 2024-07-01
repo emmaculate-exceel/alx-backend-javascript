89481d37c128b9a355970c29ea9b198a65e64ecf
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-01 18:41:48 +0100

    update

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 export default function getListStudents() {
- const arr = [
+  const arr = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'columbia' },
     { id: 5, firstName: 'Serena', loction: 'San Francisco' },
- ];
- return arr
+  ];
+  return arr;
 }
