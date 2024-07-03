b309f7277f92b816875adebe62fd5ef8a39436f0
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-03 05:44:28 +0100

    student-ids

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-function getStudentIdsSum(students) {
+export default function getStudentIdsSum(students) {
   const sum = students.reduce((accumulator, student) => accumulator + student.id, 0);
   return sum;
 }
