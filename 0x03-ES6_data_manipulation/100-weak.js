5fc78b2dfb1d85d8be1c2d5a14d010fc4edc3546
emmaculate-exceel <emmaculate2exceel@live.com>
2024-07-03 05:05:16 +0100

    update 100 again

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,12 +3,12 @@ const weakMap = new WeakMap();
 export { weakMap };
 
 export function queryAPI(endpoint) {
-    let currentCount = weakMap.get(endpoint) || 0;
-    weakMap.set(endpoint, currentCount + 1);
-    
-    if (weakMap.get(endpoint) >= 5) {
-        throw new Error('Endpoint load is high');
-    }
-    
-    return weakMap.get(endpoint);
+  const currentCount = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, currentCount + 1);
+
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  return weakMap.get(endpoint);
 }
